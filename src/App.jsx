2f83c2f5525771addc32e2b1007d76161c600ac4
md shahjalal <shahjalal.freelancer@gmail.com>
2023-12-22 01:46:20 +1400

    text zinc

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function App() {
   return (
     <div className="relative w-full h-screen bg-zinc-800 ">
-      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter">
-        Docs
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] font-semibold text-zinc-900 leading-none tracking-tighter">
+        Docs.
       </h1>
     </div>
   );
