bf3762dcd96b3a73aba1a91b54f25c69e25bb19c
md shahjalal <shahjalal.freelancer@gmail.com>
2023-12-21 17:42:13 +0600

    docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function App() {
-  return <div className="w-full h-screen bg-zinc-800 "></div>;
+  return (
+    <div className="relative w-full h-screen bg-zinc-800 ">
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter">
+        Docs
+      </h1>
+    </div>
+  );
 }
 
 export default App;
