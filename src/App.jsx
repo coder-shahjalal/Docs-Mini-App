17dba0c4ccabdc0c278b285e7b4313f2f3d4a72a
md shahjalal <shahjalal.freelancer@gmail.com>
2023-12-25 03:09:50 +1400

    sha

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] font-semibold text-zinc-900 leading-none tracking-tighter">
         Docs.
       </h1>
+      .
     </div>
   );
 }
