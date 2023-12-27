ae5fc051301e5cac17a4b67af990a79a514d021d
md shahjalal <shahjalal.freelancer@gmail.com>
2023-12-26 23:03:30 -1200

    docss delet

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
       <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] font-semibold text-zinc-900 leading-none tracking-tighter">
         Docs.
       </h1>
-      <h1>docs</h1>
     </div>
   );
 }
