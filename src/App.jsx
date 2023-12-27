198a42c62632455ddb47f5db3f921631457796a9
md shahjalal <shahjalal.freelancer@gmail.com>
2023-12-27 17:36:49 +0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="relative w-full h-screen bg-zinc-800 ">
       <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] font-semibold text-zinc-900 leading-none tracking-tighter">
-        Docs.
+        Docs..
       </h1>
     </div>
   );
