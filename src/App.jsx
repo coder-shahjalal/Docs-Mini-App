204b0a637f3e5a512a41a64bfe2597ed3a17f0f6
md shahjalal <shahjalal.freelancer@gmail.com>
2023-12-28 15:40:06 +0600

    card design ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import Background from "./components/Background";
+import Foreground from "./components/Foreground";
 
 function App() {
   return (
     <div className="relative w-full h-screen bg-zinc-800 ">
-      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] font-semibold text-zinc-900 leading-none tracking-tighter">
-        Docs..
-      </h1>
+      <Background />
+      <Foreground />
     </div>
   );
 }
---
+++ src/components/Foreground.jsx
@@ -0,0 +1,11 @@
+import Card from "./Card";
+
+function Foreground() {
+  return (
+    <div className="fixed top-0 left-0 z-[3] w-full h-full ">
+      <Card />
+    </div>
+  );
+}
+
+export default Foreground;
