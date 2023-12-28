35ad5212ee8ac6464aa4fc02e5de90a3d879abab
md shahjalal <shahjalal.freelancer@gmail.com>
2023-12-28 18:12:31 +0600

    motion card ready

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,43 @@
+import { motion } from "framer-motion";
+import React from "react";
 import { FaRegFileLines } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
 import { LuDownload } from "react-icons/lu";
 
-function Card() {
+function Card({ data, reference }) {
   return (
-    <div className="relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
+    <motion.div
+      drag
+      dragConstraints={reference}
+      whileDrag={{ scale: 1.1 }}
+      dragElastic={0.1}
+      dragTransition={{ bounceStiffness: 100, bounceDamping: 30 }}
+      className="relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden"
+    >
       <FaRegFileLines />
-      <p className="text-sm leading-tight pt-5 font-semibold">
-        Lorem ipsum dolor sit, amet consectetur adipisicing.
-      </p>
+      <p className="text-sm leading-tight pt-5 font-semibold">{data.desc}</p>
       <div className="footer absolute bottom-0 w-full left-0">
         <div className="flex items-center justify-between px-8  mb-3">
-          <h5>.4mb</h5>
+          <h5>{data.filesize}</h5>
           <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center">
-            <LuDownload size=".7em" color="#fff" />
+            {data.close ? (
+              <IoMdClose />
+            ) : (
+              <LuDownload size=".7em" color="#fff" />
+            )}
           </span>
         </div>
-        <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
-          <h3 className="text-sm font-semibold">Download Now</h3>
-        </div>
+        {data.tag.isOpen && (
+          <div
+            className={`tag w-full py-4 ${
+              data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"
+            } flex items-center justify-center`}
+          >
+            <h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
+          </div>
+        )}
       </div>
-    </div>
+    </motion.div>
   );
 }
 
